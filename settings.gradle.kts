6cbae3a152a1f9d29a869dc8994c673e604ebeea
Welsey <69085535+Welsey0@users.noreply.github.com>
2025-10-09 12:29:21 -0700

    fix: minor syntax error

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,10 +24,10 @@ stonecutter {
 
         vcsVersion = "1.21.5-fabric"
 
-        vers("1.21.8-fabric", "1.21.8"),
-        vers("1.21.8-neo", "1.21.8"),
+        vers("1.21.8-fabric", "1.21.8")
+        vers("1.21.8-neo", "1.21.8")
 
-        vers("1.21.9-fabric", "1.21.9"),
+        vers("1.21.9-fabric", "1.21.9")
         vers("1.21.9-neo", "1.21.9")
     }
 }
